8d43327f0067e52e819f27cecf1cac97a6d0dda9
Rodrigo Aguiar <rodrigo-r_@hotmail.com>
2022-07-22 03:46:19 -0300

    Further changes on the behavior of the "delete" and "check" buttons

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const toDoItems = document.querySelector(".todo-container");
 
 //EVENT LISTENERS
 addBtn.addEventListener("click", addItem);
+toDoItems.addEventListener("click", removeItem);
 
 //FUNCTIONS
 function addItem(event) {
@@ -19,16 +20,25 @@ function addItem(event) {
   newDiv.appendChild(newTodo);
   //Checked button
   const checkedBtn = document.createElement("button");
-  checkedBtn.innerHTML = '<i class="fa-solid fa-square-check"></i>';
+  checkedBtn.innerHTML = '<i id="check" class="fa-solid fa-square-check"></i>';
   checkedBtn.classList.add("checked-button");
   newDiv.appendChild(checkedBtn);
   //Delete button
   const deleteItem = document.createElement("button");
-  deleteItem.innerHTML = '<i class="fa-solid fa-trash-can"></i>';
+  deleteItem.innerHTML = '<i id="trash" class="fa-solid fa-trash-can"></i>';
   deleteItem.classList.add("delete-item");
   newDiv.appendChild(deleteItem);
 
   document.querySelector(".todo-container").appendChild(newDiv);
 
   newItem.value = "";
+}
+
+//Remove item function
+function removeItem(e) {
+  const item = e.target;
+  if (item.classList[0] === "delete-item") {
+    const todo = item.parentElement;
+    todo.remove();
+  }
 }
